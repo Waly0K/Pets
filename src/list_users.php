4c2b5d4a86815f6911ab370f4b8f0626988abccb
Waly <sebaseraso13@gmail.com>
2024-05-31 22:50:00 -0500

    borrar usuario

--- src/list_users.php
+++ src/list_users.php
@@ -26,6 +26,18 @@ require '../config/database.php';
             <th>...</th>
         </tr>
         <?php 
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete_user'])) {
+                $user_id = $_POST['user_id'];
+                $query_delete_user = "DELETE FROM users WHERE id = $user_id";
+                $result_delete = pg_query($conn, $query_delete_user);
+                if ($result_delete) {
+                    echo "<div class='alert alert-success' role='alert'>Usuario eliminado satisfactoriamente.</div>";
+                } else {
+                    echo "<div class='alert alert-danger' role='alert'>Error al eliminar el usuario.</div>";
+                }
+            }
+
             $query_users = "SELECT ID, FULLNAME, EMAIL, CASE WHEN STATUS = TRUE THEN 'ACTIVO' ELSE 'INACTIVO' END AS STATUS FROM USERS";
             $result = pg_query($conn, $query_users);
             while($row = pg_fetch_assoc($result)){
@@ -34,14 +46,21 @@ require '../config/database.php';
                 echo "<td>".$row['email']."</td>";
                 echo "<td>".$row['status']."</td>";
                 echo "<td><img src='Photos/usuario.png' width='32'></td>";
-                echo "<td>
-                <a href='#'><img src='iconos/Editar.png' width='20'></a>
-                <a href='delete_user.php?id=' onclick='return confirm(\"Are you sure you want to delete this user?\")'><img src='iconos/borrar.png' width='20'></a>
-            </td>";
-        echo "</tr>";
+                echo "<td> <a href='#'><img src='iconos/editar.png' width='32'></a> 
+                <form method='POST' onsubmit='return confirmDelete()'>
+                <input type='hidden' name='user_id' value='". $row['id'] ."'>
+                <button type='submit' name='delete_user' class='btn btn-danger btn-sm'><img src='iconos/borrar.png' width='20'></button>
+                </form> 
+                </td>";
+                echo "</tr>";
             }
         ?>
     </table>
 </div>
+<script>
+    function confirmDelete() {
+        return confirm("Estas seguro de eliminar el usuario?");
+    }
+</script>
 </body>
 </html>
